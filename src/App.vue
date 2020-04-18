c8c8fff1db709d64b9e7282aa3f772dfd051d5e3
tboyd4 <tboyd1898@gmail.com>
2020-04-18 18:43:06 -0500

    button added to allow user to toggle theme

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 			>
 				{{ link.label }}
 			</v-btn>
+			<v-btn @click="toggleTheme" text rounded>Toggle Theme</v-btn>
 		</v-app-bar>
 		<v-content>
 			<router-view></router-view>
@@ -60,6 +61,11 @@ export default {
 					url: '/signup'
 				}
 			]
+		}
+	},
+	methods: {
+		toggleTheme() {
+			this.$vuetify.theme.dark = !this.$vuetify.theme.dark
 		}
 	}
 }
