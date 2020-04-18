0be8b1f37ca3fd1100e14713a136758e561cef63
tboyd4 <tboyd1898@gmail.com>
2020-04-18 15:25:20 -0500

    added signup page with form

---
+++ src/views/Signup.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col>
+                    <v-form>
+                        <v-text-field label="Email" type="email"></v-text-field>
+                        <v-autocomplete label="Which browser do you use?" :items="browsers"></v-autocomplete>
+                        <v-file-input label="Attach Profile Picture"></v-file-input>
+                        <v-text-field label="Birthday" v-model="birthday" readonly></v-text-field>
+                        <v-date-picker v-model="birthday" ></v-date-picker>
+                        <v-checkbox label="Agree to Terms and Conditions"></v-checkbox>
+                        <v-btn type="submit" color="primary">Submit</v-btn>
+                    </v-form>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            browsers: ['Chrome', 'Firefox', 'Safari', 'Edge', 'Brave'],
+            birthday: ''
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
