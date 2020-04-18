53e1083b6f50961413009762fbf4ec34281d53ce
tboyd4 <tboyd1898@gmail.com>
2020-04-17 22:25:24 -0500

    code to deploy to heroku added

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const PORT = process.env.PORT || 8081;
+app.listen(PORT);
